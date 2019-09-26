1b4e5b77ca5fb6c0b72669f7852bd4acd6e988d8
Adrian C. Senna <55816324+Raykitsuu@users.noreply.github.com>
2019-09-26 19:26:55 +0700

    Add files via upload

---
+++ IF1-10118036-Latihan9-Text/src/latihan9/Latihan9.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan9;
+
+/**
+ *
+ * @author
+ * NAMA			: Adrian C. Senna
+ * KELAS		: IF-1
+ * NIM			: 10118036
+ * Deskripsi Program	: Program ini berisi program untuk memperlihatkan manipulasi teks dalam pemrograman Java.
+ */
+public class Latihan9 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		char c;
+		char tab = '\t';
+		char newline = '\n';
+		c = 'A';
+		System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+	}
+	
+}
